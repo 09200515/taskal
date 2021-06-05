829f9cdf31e6ac0421bdbd871db1d1a2901c8a45
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-06-05 15:25:08 +0900

    Initial Commit

---
+++ Taskaru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taskaru
+//
+//  Created by 千葉雄太 on 2021/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
